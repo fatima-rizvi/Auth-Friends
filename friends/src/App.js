e39d836d80c99fee59cb501d793d3938c7e61eed
Fatima Rizvi <fatimarzv23@gmail.com>
2020-10-13 19:52:08 -0500

    initial array is rendering, but items are not being properly added

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import Login from './components/Login';
+import PrivateRoute from "./components/PrivateRoute";
+import FriendsList from './components/FriendsList'
 
 import './App.css';
 
@@ -14,12 +16,12 @@ function App() {
           <li>
             <Link to="/login">Login</Link>
           </li>
-          {/* <li>
+          <li>
             <Link to="/protected">Protected Page</Link>
-          </li> */}
+          </li>
         </ul>
         <Switch>
-          {/* <PrivateRoute exact path="/protected" component={GasPrices} /> */}
+          <PrivateRoute exact path="/protected" component={FriendsList} />
           <Route path="/login" component={Login} />
           <Route component={Login} />
         </Switch>
---
+++ friends/src/components/AddFriend.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from 'react';
+import { useForm } from '../hooks/useForm';
+import axios from 'axios';
+import { render } from 'react-dom';
+import { useHistory } from 'react-router-dom';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+const initialValues = {
+    name: '',
+    age: '',
+    email: ''
+}
+
+const AddFriend = (props) => {
+    // states
+    const [formValues, handleChanges, resetForm, setValues] = useForm(initialValues);
+    
+    //console.log("Props: ", props)
+
+    //Helpers
+
+    let history = useHistory();
+
+    const postNewFriend = newFriend => {
+        axiosWithAuth()
+            .post('/friends', newFriend)
+            .then((res) => {
+                console.log('add friends: ', res)
+                props.setFriends({
+                    friends: res.data
+                })
+            })
+            .catch((err) => {
+                console.log('Error: ', err)
+            })
+    }
+
+    //side-effects
+
+  const submitValues = evt => {
+    evt.preventDefault();
+    const newFriend = {
+      id: Math.random(),  
+      name: formValues.name.trim(),
+      age: formValues.age.trim(),
+      email: formValues.email.trim(),
+    };
+    postNewFriend(newFriend);
+    resetForm(initialValues);
+  };
+
+  return (
+    <div>
+            <h1>Add Friend</h1>
+            <form className = 'team-form' onSubmit = {submitValues}>
+                <label>Name: 
+                    <input 
+                        type = 'text'
+                        name = 'name'
+                        onChange = {handleChanges}
+                        value = {formValues.name}
+                        placeholder = 'enter name'
+                    />
+                </label>
+
+                <label>Age: 
+                    <input 
+                        type = 'numer'
+                        name = 'age'
+                        onChange = {handleChanges}
+                        value = {formValues.age}
+                        placeholder = 'enter age'
+                    />
+                </label>
+
+                <label>Email: 
+                    <input 
+                        type = 'email'
+                        name = 'email'
+                        onChange = {handleChanges}
+                        value = {formValues.email}
+                        placeholder = 'enter email'
+                    />
+                </label>
+
+                <button>Submit</button>
+            </form>
+        </div>
+  );
+};
+
+export default AddFriend;
